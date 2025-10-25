8a68929722368afb5fd87aac8a4a1b7045ef76d3
IslombekXolboyev <islombekmoliya@gmail.com>
2025-10-25 18:14:02 +0500

    Create hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,31 @@
+# 1
+a = float(input('Enter the side of the square: '))
+perimeter = 4 * a
+area = a**2
+print('Perimetr of the square =', perimeter)
+print('Area of the square =', area)
+
+# 2
+import math
+d = float(input('Enter the diameter of the circle: '))
+length = math.pi * d
+print('Circumference length: ', length)
+
+# 3
+a = float(input('Birinchi raqamni kiriting: '))
+b = float(input('Ikkinchi raqamni kiriting: '))
+mean = (a+b)/2
+print("O'rta arifmetigi: ", mean)
+
+# 4
+a = float(input('Birinchi raqamni kiriting: '))
+b = float(input('Ikkinchi raqamni kiriting: '))
+
+summa = a + b
+kupaytma = a*b
+a_ning_kvadrati = a**2
+b_ning_kvadrati = b**2
+print("Raqamlar yig'indisi: ", summa)
+print("Raqamlar ko'paytmasi: ", kupaytma)
+print("Birinchi raqam kvadrati: ", a_ning_kvadrati)
+print("Ikkinchi raqam kvadrati: ", b_ning_kvadrati)
